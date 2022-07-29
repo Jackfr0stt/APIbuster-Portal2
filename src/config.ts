cc46eb07bd3d651e1db6c37c0fe196a02fde2664
pedro.f.sousa <pedro.f.sousa@inesctec.pt>
2022-07-29 14:01:02 +0100

    Adds docker settings

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const HOSTNAME = "http://localhost";
-export const API_URL = "http://localhost:8080";
+export const HOSTNAME = "http://apibuster-service";
+export const API_URL = "http://apibuster-service:8080";
