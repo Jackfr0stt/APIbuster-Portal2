a59ab3248da8a36d41095aef462a9ae7cce38be8
pedro.f.sousa <pedro.f.sousa@inesctec.pt>
2022-10-04 18:28:07 +0100

    Fixes sidebar size

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,25 +1,25 @@
 <template>
   <aside :class="`${is_expanded && 'is_expanded'}`">
     <div class="logo">
-        <img src="../assets/logo.png" alt="Vue">
+      <img src="../assets/logo.png" alt="Vue" />
     </div>
 
     <div class="menu-toggle-wrap">
-        <div class="button menu-toggle" @click="ToggleMenu">
-            <span class="material-icons">keyboard_double_arrow_right</span>
-        </div>
+      <div class="button menu-toggle" @click="ToggleMenu">
+        <span class="material-icons">keyboard_double_arrow_right</span>
+      </div>
     </div>
-    
+
     <h3>Menu</h3>
     <div class="menu">
-        <!-- <router-link class="button" to="/">
-            <span class="material-icons">home</span>
-            <span class="text">Home</span>
-        </router-link> -->
-        <router-link class="button" to="/apis">
-            <span class="material-icons">api</span>
-            <span class="text">APIs</span>
-        </router-link>
+      <router-link class="button" to="/">
+        <span class="material-icons">home</span>
+        <span class="text">Home</span>
+      </router-link>
+      <router-link class="button" to="/apis">
+        <span class="material-icons">api</span>
+        <span class="text">APIs</span>
+      </router-link>
     </div>
 
     <div class="flex"></div>
@@ -34,141 +34,146 @@
 </template>
 
 <script setup>
-import {ref} from 'vue'
+import { ref } from "vue";
 
 const is_expanded = ref(localStorage.getItem("is_expanded") === "true");
 
 const ToggleMenu = () => {
-    is_expanded.value = !is_expanded.value;
+  is_expanded.value = !is_expanded.value;
 
-    localStorage.setItem("is_expanded", is_expanded.value)
-}
+  localStorage.setItem("is_expanded", is_expanded.value);
+};
 </script>
 
 <style lang="scss" scoped>
 aside {
+  display: flex;
+  flex-direction: column;
+  width: calc(2rem + 2rem);
+  min-height: 100vh;
+  overflow: hidden;
+  padding: 1rem;
+
+  background-color: var(--dark);
+  color: var(--light);
+
+  transition: 0.2s ease-out;
+
+  .flex {
+    flex: 1 1 0;
+  }
+
+  .logo {
+    margin-bottom: 1rem;
+
+    img {
+      width: 2rem;
+    }
+  }
+
+  .menu-toggle-wrap {
     display: flex;
-    flex-direction: column;
-    width: calc(2rem + 2rem);
-    min-height: 100vh;
-    overflow: hidden;
-    padding: 1rem;
+    justify-content: flex-end;
+    margin-bottom: 1rem;
 
-    background-color: var(--dark);
-    color: var(--light);
-
+    position: relative;
+    top: 0;
     transition: 0.2s ease-out;
+    .menu-toggle {
+      transition: 0.2s ease-out;
 
-    .flex {
-        flex: 1 1 0;
-    }
+      .material-icons {
+        font-size: 2rem;
+        color: var(--light);
+      }
 
-    .logo {
-        margin-bottom: 1rem;
-
-        img {
-            width: 2rem;
+      &:hover {
+        .material-icons {
+          color: var(--primary);
+          transform: translateX(0.5rem);
+          transition: 0.2s ease-out;
         }
+      }
     }
+  }
+
+  h3,
+  .button .text {
+    opacity: 0;
+    transition: 0.3s ease-out;
+  }
+
+  h3 {
+    color: var(--grey);
+    font-size: 0.875rem;
+    margin-bottom: 0.5rem;
+    text-transform: uppercase;
+  }
+  .menu {
+    margin: 0 -1rem;
+
+    .button {
+      display: flex;
+      align-items: center;
+      text-decoration: none;
+
+      padding: 0.5rem 1rem;
+      transition: 0.2s ease-out;
+
+      .material-icons {
+        font-size: 2rem;
+        color: var(--light);
+        transition: 0.2s ease-out;
+      }
+
+      .text {
+        color: var(--light);
+        transition: 0.2s ease-out;
+      }
+
+      &:hover,
+      &.router-link-exact-active {
+        background-color: var(--dark-alt);
+
+        .material-icons,
+        .text {
+          color: var(--primary);
+        }
+      }
+
+      &.router-link-exact-active {
+        border-right: 5px solid var(--primary);
+      }
+    }
+  }
+
+  &.is_expanded {
+    width: 8rem;
 
     .menu-toggle-wrap {
-        display: flex;
-        justify-content: flex-end;
-        margin-bottom: 1rem;
+      top: -0.25rem;
 
-        position: relative;
-        top: 0;
-        transition: 0.2s ease-out;
-        .menu-toggle {
-            transition: 0.2s ease-out;
-
-            .material-icons {
-                font-size: 2rem;
-                color: var(--light);
-            }
-
-            &:hover {
-                .material-icons {
-                    color: var(--primary);
-                    transform: translateX(0.5rem);
-                    transition: 0.2s ease-out;
-                }
-            }
-        }
+      .menu-toggle {
+        transform: rotate(-180deg);
+      }
     }
 
-    h3, .button .text {
-        opacity: 0;
-        transition: 0.3s ease-out;
+    h3,
+    .button .text {
+      opacity: 1;
+      transition: 0.3s ease-out;
     }
 
-    h3 {
-        color: var(--grey);
-        font-size: 0.875rem;
-        margin-bottom: 0.5rem;
-        text-transform: uppercase;
+    .button {
+      .material-icons {
+        margin-right: 1rem;
+      }
     }
-    .menu {
-        margin: 0 -1rem;
+  }
 
-        .button {
-            display: flex;
-            align-items: center;
-            text-decoration: none;
-
-            padding: 0.5rem 1rem;
-            transition: 0.2s ease-out;
-
-            .material-icons {
-                font-size: 2rem;
-                color: var(--light);
-                transition: 0.2s ease-out;
-            }
-
-            .text {
-                color: var(--light);
-                transition: 0.2s ease-out;
-            }
-
-            &:hover, &.router-link-exact-active {
-                background-color: var(--dark-alt);
-
-                .material-icons, .text {
-                    color: var(--primary);
-                }
-            }
-
-            &.router-link-exact-active {
-                border-right: 5px solid var(--primary);
-            }
-        }
-    }
-
-    &.is_expanded {
-        width: var(--sidebar-width);
-
-        .menu-toggle-wrap {
-            top: -3rem;
-            .menu-toggle {
-                transform: rotate(-180deg);
-            }
-        }
-
-        h3, .button .text {
-            opacity: 1;
-            transition: 0.3s ease-out;
-        }
-
-        .button {
-            .material-icons {
-                margin-right: 1rem;
-            }
-        }
-    }
-
-    @media (max-width: 768px) {
-        position: fixed;
-        z-index: 99;
-    }
+  @media (max-width: 768px) {
+    position: fixed;
+    z-index: 99;
+  }
 }
 </style>
--- src/views/Apis.vue
+++ src/views/Apis.vue
@@ -1,217 +1,254 @@
 <template>
-    <main class="API-page">
-        <Title title="APIs" />
+  <main class="API-page">
+    <Title title="APIs" />
 
-        <!-- API CARDS -->
-        <section class="info">
-            <!-- <img src="https://codetheweb.blog/assets/img/icon2.png">
+    <!-- API CARDS -->
+    <section class="info">
+      <!-- <img src="https://codetheweb.blog/assets/img/icon2.png">
             <h1>Learn HTML &mdash; <a href="https://codetheweb.blog/" target="_blank">Code The Web</a></h1> -->
-        </section>
+    </section>
 
-        <section class="cards-wrapper">
-            <div v-for="api in apis">
-                <div class="card-grid-space">
-                    <a class="card" v-if="!api.edit"
-                        style="--bg-img: url('https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&resize_w=1500&url=https://codetheweb.blog/assets/img/posts/html-syntax/cover.jpg')">
-
-                        <div>
-                            <h1>{{ api.apiName }}</h1>
-                            <p>{{ api.apiDomain }}</p>
-                            <div class="tags">
-                                <div class="tag">{{ api.apiType }}</div>
-                                <router-link class="button" :to="`/apis/${api.id}/methods`">
-                                    <span class="text">Open</span>
-                                </router-link>
-                            </div>
-                        </div>
-                        <span class="material-icons update-api" @click="editAPI(api.id)">edit</span>
-                        <span class="material-icons delete-api" @click="delAPI(api.id)">delete</span>
-                    </a>
-                    <a class="card-update" v-if="api.edit">
-                        <div>
-                            <input type="text" id="updatedApiName" :placeholder="api.apiName" class="api-input">
-                            <input type="text" id="updatedApiDomain" :placeholder="api.apiDomain" class="domain-input">
-                            <div class="tags">
-                                <div class="tag">
-                                    <select id="updatedApiType" class="tag-select">
-                                        <option value="REST">REST</option>
-                                        <option value="RPC" disabled>RPC</option>
-                                    </select>
-                                </div>
-                                <span class="button" @click="updateAPI(api)">Update</span>
-                            </div>
-                        </div>
-                        <span class="material-icons cancel-update" @click="editAPI(api.id)">cancel</span>
-                    </a>
-                </div>
+    <section class="cards-wrapper">
+      <div v-for="api in apis">
+        <div class="card-grid-space">
+          <a
+            class="card"
+            v-if="!api.edit"
+            style="
+              --bg-img: url('https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&resize_w=1500&url=https://codetheweb.blog/assets/img/posts/html-syntax/cover.jpg');
+            "
+          >
+            <div>
+              <h1>{{ api.apiName }}</h1>
+              <p>{{ api.apiDomain }}</p>
+              <div class="tags">
+                <div class="tag">{{ api.apiType }}</div>
+                <router-link class="button" :to="`/apis/${api.id}/methods`">
+                  <span class="text">Open</span>
+                </router-link>
+              </div>
             </div>
-
-            <div v-if="expanded">
-                <div class="card-grid-space">
-                    <a class="card-update">
-                        <div>
-                            <input type="text" id="apiName" placeholder="API name" class="api-input">
-                            <input type="text" id="apiDomain" placeholder="https://<web_link>" class="domain-input">
-                            <div class="tag" style="margin-bottom: 0.2em;">
-                                <input type="file" @change="uploadFile()" ref="file">
-                            </div>
-                            <div class="tags">
-                                <div class="tag">
-                                    <select id="apiType" class="tag-select">
-                                        <option value="REST">REST</option>
-                                        <option value="RPC" disabled>RPC</option>
-                                    </select>
-                                </div>
-                                <span class="button" @click="newAPI()">Create</span>
-                            </div>
-                        </div>
-                        <span class="material-icons cancel-update" @click="ToggleApi()">cancel</span>
-                    </a>
+            <span class="material-icons update-api" @click="editAPI(api.id)"
+              >edit</span
+            >
+            <span class="material-icons delete-api" @click="delAPI(api.id)"
+              >delete</span
+            >
+          </a>
+          <a class="card-update" v-if="api.edit">
+            <div>
+              <input
+                type="text"
+                id="updatedApiName"
+                :placeholder="api.apiName"
+                class="api-input"
+              />
+              <input
+                type="text"
+                id="updatedApiDomain"
+                :placeholder="api.apiDomain"
+                class="domain-input"
+              />
+              <div class="tags">
+                <div class="tag">
+                  <select id="updatedApiType" class="tag-select">
+                    <option value="REST">REST</option>
+                    <option value="RPC" disabled>RPC</option>
+                  </select>
                 </div>
+                <span class="button" @click="updateAPI(api)">Update</span>
+              </div>
             </div>
+            <span class="material-icons cancel-update" @click="editAPI(api.id)"
+              >cancel</span
+            >
+          </a>
+        </div>
+      </div>
 
-            <div class="card-grid-space">
-                <div class="new-card center" @click="ToggleApi()">
-                    <span class="material-icons" v-if="!expanded">add_circle</span>
+      <div v-if="expanded">
+        <div class="card-grid-space">
+          <a class="card-update">
+            <div>
+              <input
+                type="text"
+                id="apiName"
+                placeholder="API name"
+                class="api-input"
+              />
+              <input
+                type="text"
+                id="apiDomain"
+                placeholder="https://<web_link>"
+                class="domain-input"
+              />
+              <div class="tag" style="margin-bottom: 0.2em">
+                <input type="file" @change="uploadFile()" ref="file" />
+              </div>
+              <div class="tags">
+                <div class="tag">
+                  <select id="apiType" class="tag-select">
+                    <option value="REST">REST</option>
+                    <option value="RPC" disabled>RPC</option>
+                  </select>
                 </div>
+                <span class="button" @click="newAPI()">Create</span>
+              </div>
             </div>
-        </section>
+            <span class="material-icons cancel-update" @click="ToggleApi()"
+              >cancel</span
+            >
+          </a>
+        </div>
+      </div>
 
-    </main>
+      <div class="card-grid-space">
+        <div class="new-card center" @click="ToggleApi()">
+          <span class="material-icons" v-if="!expanded">add_circle</span>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script>
-import Title from '../components/Title.vue'
-import { wrapper } from '../utils/wrapper';
-import ApiService from '../services/Apiservice';
+import Title from "../components/Title.vue";
+import { wrapper } from "../utils/wrapper";
+import ApiService from "../services/Apiservice";
 
 let apiService = new ApiService();
 
 export default {
-    data() {
-        return {
-            apis: this.created(),
-            file: '',
-            expanded: false
-        };
+  data() {
+    return {
+      apis: this.created(),
+      file: "",
+      expanded: false,
+    };
+  },
+  methods: {
+    async created() {
+      const res = await wrapper(apiService.getAPIs());
+      this.apis = res.data || [];
+      console.log(this.apis);
+      this.apis.forEach((api) => {
+        api.edit = false;
+      });
     },
-    methods: {
-        async created() {
-            const res = await wrapper(apiService.getAPIs());
-            this.apis = res.data;
-            this.apis.forEach(api => {
-                api.edit = false;
-            });
-        },
-        async ToggleApi() {
-            this.expanded = !this.expanded;
-        },
-        async newAPI() {
-            const API = {
-                apiName: document.getElementById("apiName").value,
-                apiDomain: document.getElementById("apiDomain").value,
-                apiType: document.getElementById("apiType").value
-            };
+    async ToggleApi() {
+      this.expanded = !this.expanded;
+    },
+    async newAPI() {
+      const API = {
+        apiName: document.getElementById("apiName").value,
+        apiDomain: document.getElementById("apiDomain").value,
+        apiType: document.getElementById("apiType").value,
+      };
 
-            const res = await wrapper(apiService.newAPI(API));
-            this.apis.push(res.data);
+      const res = await wrapper(apiService.newAPI(API));
+      this.apis.push(res.data);
 
-            await this.submitFile(res.data.id);
+      await this.submitFile(res.data.id);
 
-            this.expanded = !this.expanded;
-        },
-        async uploadFile() {
-            this.file = this.$refs.file.files[0];
-        },
-        async editAPI(id) {
-            await this.apis.forEach(api => {
-                if (api.id == id) {
-                    api.edit = !api.edit;
-                }
-            });
-        },
-        async updateAPI(api) {
-            const updateAPI = {
-                apiName: await document.getElementById("updatedApiName").value || api.apiName,
-                apiDomain: await document.getElementById("updatedApiDomain").value || api.apiDomain,
-                apiType: await document.getElementById("updatedApiType").value || api.apiType
-            };
-
-            await this.apis.forEach(API => {
-                if (API.id == api.id) {
-                    API.edit = !API.edit;
-                }
-            });
-
-            await wrapper(apiService.updateAPI(api.id, updateAPI));
-            const updatedAPI = await wrapper(apiService.getAPIById(api.id));
-            if (updatedAPI.error) {
-                throw updatedAPI.error;
-            }
-
-            if (updatedAPI.data) {
-                let count = 0;
-                for (let api of this.apis) {
-                    if (api.id == updatedAPI.data.id) {
-                        this.apis[count] = updatedAPI.data;
-                    }
-                    count++;
-                }
-            }
-        },
-        async submitFile(id) {
-            let formData = new FormData();
-            formData.append('file', this.file);
-            await wrapper(apiService.sendFile(id, formData));
-        },
-        async delAPI(id) {
-            await wrapper(apiService.delAPI(id));
-            location.reload();
+      this.expanded = !this.expanded;
+    },
+    async uploadFile() {
+      this.file = this.$refs.file.files[0];
+    },
+    async editAPI(id) {
+      await this.apis.forEach((api) => {
+        if (api.id == id) {
+          api.edit = !api.edit;
         }
+      });
     },
-    components: { Title }
-}
+    async updateAPI(api) {
+      const updateAPI = {
+        apiName:
+          (await document.getElementById("updatedApiName").value) ||
+          api.apiName,
+        apiDomain:
+          (await document.getElementById("updatedApiDomain").value) ||
+          api.apiDomain,
+        apiType:
+          (await document.getElementById("updatedApiType").value) ||
+          api.apiType,
+      };
 
+      await this.apis.forEach((API) => {
+        if (API.id == api.id) {
+          API.edit = !API.edit;
+        }
+      });
+
+      await wrapper(apiService.updateAPI(api.id, updateAPI));
+      const updatedAPI = await wrapper(apiService.getAPIById(api.id));
+      if (updatedAPI.error) {
+        throw updatedAPI.error;
+      }
+
+      if (updatedAPI.data) {
+        let count = 0;
+        for (let api of this.apis) {
+          if (api.id == updatedAPI.data.id) {
+            this.apis[count] = updatedAPI.data;
+          }
+          count++;
+        }
+      }
+    },
+    async submitFile(id) {
+      let formData = new FormData();
+      formData.append("file", this.file);
+      await wrapper(apiService.sendFile(id, formData));
+    },
+    async delAPI(id) {
+      await wrapper(apiService.delAPI(id));
+      location.reload();
+    },
+  },
+  components: { Title },
+};
 </script>
 
 <style lang="scss">
 @import "Apis.scss";
 
 .update-api {
-    position: absolute;
-    top: 0.75em;
-    right: 1em;
-    margin: none;
-    font-size: 1.25em;
+  position: absolute;
+  top: 0.75em;
+  right: 1em;
+  margin: none;
+  font-size: 1.25em;
 
-    &:hover {
-        cursor: pointer;
-    }
+  &:hover {
+    cursor: pointer;
+  }
 }
 
 .cancel-update {
-    position: absolute;
-    opacity: 0.8;
-    top: 0.75em;
-    right: 1em;
-    margin: none;
-    font-size: 1.25em;
-    color: var(--color);
+  position: absolute;
+  opacity: 0.8;
+  top: 0.75em;
+  right: 1em;
+  margin: none;
+  font-size: 1.25em;
+  color: var(--color);
 
-    &:hover {
-        cursor: pointer;
-    }
+  &:hover {
+    cursor: pointer;
+  }
 }
 
 .delete-api {
-    position: absolute;
-    top: 2em;
-    right: 0.75em;
-    margin: none;
-    font-size: 1.5em;
+  position: absolute;
+  top: 2em;
+  right: 0.75em;
+  margin: none;
+  font-size: 1.5em;
 
-    &:hover {
-        cursor: pointer;
-    }
+  &:hover {
+    cursor: pointer;
+  }
 }
 </style>
