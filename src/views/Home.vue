4019db3519ff00b86e673159b36674620b8fb8e8
pedro.f.sousa <pedro.f.sousa@inesctec.pt>
2022-10-11 22:26:52 +0100

    Updates Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,31 @@
 	<main class="home-page">
 		<Title />
 		<div class="view_text">
-			<p>Welcome to APIbuster app.</p>
-			<p>With this app you can test your API real-time.</p>
+			<p>Welcome to APIbuster user dashboard.</p>
+			<p>With this app you can test your API in real-time.</p><br>
+
+			<p>Take into consideration that all tests must be written using mocha and chai.</p><br>
+			
+			<p>Please visit <a href="https://mochajs.org/">MochaJS</a> website for examples.</p><br><br>
+			
+			<p>You only need to worry about creating the test itself. For example:</p>
+				
+			<pre>
+				<code>
+					it('respond with matching records', function (done) {
+						db.find({type: 'User'}, function (err, res) {
+							if (err) return done(err);
+							res.should.have.length(3);
+							done();
+						});
+					});
+				</code>
+			</pre>
+			
+			<p>In this test you're expecting the return value of the function to have at least a length equal to 3.</p><br><br>
+
+			<p>Proceed to API page using the sidebar and create your own tests.</p><br>
+
 		</div>
 	</main>
 </template>
@@ -16,5 +39,13 @@ import Title from '../components/Title.vue'
 .view_text {
 	margin-top: 10rem;
 	font-size: 1.5rem;
+}
+
+pre code {
+  background-color: #eee;
+  border: 1px solid #999;
+  display: block;
+	width: 1050px;
+  padding: 5px;
 }
 </style>
